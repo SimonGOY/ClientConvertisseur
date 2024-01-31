9f64a5b135f83a92d6ee2d65975bb8534deb8cb6
SIMON GOY <sg.bonneville@gmail.com>
2024-01-31 08:58:47 +0100

    Interace peut récupérer les Devises depuis l'API

--- ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
+++ ClientConvertisseurV1/Views/ConvertisseurEuroPage.xaml.cs
@@ -1,18 +1,11 @@
 using ClientConvertisseurV1.Services;
-using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.Services.Maps;
 using WSConvertisseur.Models;
 
 // To learn more about WinUI, the WinUI project structure,
@@ -23,23 +16,85 @@ namespace ClientConvertisseurV1.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class ConvertisseurEuroPage : Page
+    public sealed partial class ConvertisseurEuroPage : Page, INotifyPropertyChanged
     {
+        private ObservableCollection<Devise> devises;
+
+        public ObservableCollection<Devise> Devises
+        {
+            get { return devises; }
+            set
+            {
+                devises = value;
+            }
+        }
+
+        private Devise selectedDevise;
+
+        public Devise SelectedDevise { 
+            get { return selectedDevise; }
+            set { 
+                selectedDevise = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+        private double montantEuro;
+
+        public double MontantEuro
+        {
+            get { return montantEuro; }
+            set
+            {
+                montantEuro = Math.Round(value, 2);
+                this.OnPropertyChanged();
+            }
+        }
+
+        private double montantDevise;
+        public double MontantDevise
+        {
+            get { return montantDevise; }
+            set
+            {
+                montantDevise = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
         public ConvertisseurEuroPage()
         {
             this.InitializeComponent();
+            Devises = new ObservableCollection<Devise>();
             this.DataContext = this;
+            
             GetDataOnLoadAsync();
         }
 
         private async void GetDataOnLoadAsync()
         {
-            WSService service = new WSService("https://localhost/#PORT/api/");
+            WSService service = new WSService("https://localhost:44389/api/");
             List<Devise> result = await service.GetDevisesAsync("devises");
             if (result == null)
             {
                 //MessageAsync("API non disponible !", "Erreur");
             }
+            else
+            {
+                foreach (Devise dev in result) 
+                {
+                    Devises.Add(dev);
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
